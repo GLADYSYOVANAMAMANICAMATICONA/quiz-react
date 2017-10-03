861b3b22eb2bc14421144127df4f05eac1ee4f3a
GLADYSYOVANAMAMANICAMATICONA <gladysyovanamamanicamaticona@gmail.com>
2017-10-03 08:10:43 -0500

    progressbar

--- app.jsx
+++ app.jsx
@@ -3,6 +3,7 @@ let preguntas = [
     pregunta: "Tipos de variables en ECMAScript v6",
     alternativas: ["let - const", "let - var", "const - var"],
     imagen: "img/img3.jpg",
+    bar: "img/bar.png",
     respuesta: 0
   },
   {
@@ -10,12 +11,14 @@ let preguntas = [
     pregunta: "Abreviado de ECMAScript v6",
     alternativas: ["ES6 - ES2015", "ES6", "ES2015"],
     imagen: "img/img1.png",
+    bar: "img/bar1.png",
     respuesta: 0
   },
   {
     pregunta: "La fecha de la publicación ECMAScript v6",
     alternativas: ["Junio 2015", "Junio 2016", "Julio 2015"],
     imagen: "img/img2.png",
+    bar: "img/bar2.png",
     respuesta: 0
   },
 ];
@@ -42,39 +45,61 @@ class Quiz extends React.Component {
   }
 
   capturarRespuesta(e) {//captura con e.target.textContent y agrega en el array concat
-    let item = {
+    /*let item = {
       respuestaSeleccionada: e.target.textContent
     };
+   */
     this.setState({
-      arrayRespuestasCliqueadas: this.state.arrayRespuestasCliqueadas.concat([item]), //se agrega en el array arrayRespuestasCliqueadas declado como global el item que es capturado 
+      //arrayRespuestasCliqueadas: this.state.arrayRespuestasCliqueadas.concat([item]), //se agrega en el array arrayRespuestasCliqueadas declado como global el item que es capturado 
       count: this.state.count + 1
     });
     console.log(this.state);
     console.log(item)
   }
 
+  mostrarAlternativas(alternativas) {
+    let alt = preguntas.alternativas
+    return (Object.keys(alternativas).map((key, index) => {//preguntas--objeto del array---- INDEX posicion del array
+      return (
+        <div className="botones">
+          <button className="btn btn-primary" key={index} onClick={(e) => this.capturarRespuesta()}>{preguntas[this.state.count].alternativas[index]}</button>
+        </div>
+      )
+    })
+    )
+  }
+  mostrarPregunta() {
+    return (
+      <div>
+        <div>
+          {preguntas[this.state.count].pregunta}
+        </div>
+        <div>
+          {this.mostrarAlternativas(preguntas[this.state.count].alternativas)}
+        </div>
+      </div>
+    )
+  }
+
   render() {//se mostrara las preguntas con sus respuestas
     return (
       <div className="preguntas scoreboard ">
         <div>{this.header()}</div>
-        {
-          preguntas.map((preguntas, index) => {//preguntas--objeto del array---- INDEX posicion del array
-            return (
-              <div className="player" key={index} >
-                <div className="player-name " >
-                  <div  > <img src={preguntas.imagen} /> </div>
-                  <p > {preguntas.pregunta} </p>
-                  <div className="">
-                    <button onClick={(e) => this.capturarRespuesta(e)} className="btn btn-primary" > {preguntas.alternativas[0]} </button>
-                    <button onClick={(e) => this.capturarRespuesta(e)} className="btn btn-primary" > {preguntas.alternativas[1]} </button>
-                    <button onClick={(e) => this.capturarRespuesta(e)} className="btn btn-primary" > {preguntas.alternativas[2]} </button>
-                  </div>
-                  <div>{this.rSociales()}</div>
-                </div>
-              </div>
-            )
-          })
-        }
+        <div>
+          <img src={preguntas[this.state.count].bar} />
+        </div>
+        <div className="player" >
+          <div className="player-name " >
+            <div>
+              <img src={preguntas[this.state.count].imagen} />
+            </div>
+            <div>
+              <div>{this.mostrarPregunta()}</div>
+              <div>{this.
+                rSociales()}</div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
@@ -83,8 +108,8 @@ class Quiz extends React.Component {
     return (
       <center>
         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 btn-social">
-          <a href="#" className="iconoFacebook" title="Facebook"><i className="fa fa-facebook"></i></a>
-          <a href="#" className="iconoYoutube" title="Youtube"><i className="fa fa-youtube "></i></a>
+          <a href="#" className="iconoFacebook" title="Facebook"><i className="fa fa-facebook-square"></i></a>
+          <a href="#" className="iconoYoutube" title="Youtube"><i className="fa fa-youtube-square "></i></a>
         </div>
       </center>
     )
